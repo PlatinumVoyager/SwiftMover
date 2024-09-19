141450142d6f7dd3c04215c086df2e16c0a4deba
Malachi McNally <116006542+PlatinumVoyager@users.noreply.github.com>
2024-09-19 13:58:21 -0400

    Create swiftheader.h

---
+++ swiftheader.h
@@ -0,0 +1,22 @@
+#ifndef SWIFTHEADER_H
+#define SWIFTHEADER_H
+
+// general headers
+#include <wchar.h>
+#include <combaseapi.h>
+
+// BITS headers
+#include <bits.h>
+#include <deliveryoptimization.h>
+
+// pragmas 
+#pragma comment(lib, "bits.lib")
+
+// https://learn.microsoft.com/en-us/windows/win32/delivery_optimization/ibackgroundcopymanager#methods
+// #pragma comment(lib, "dosvc.lib")
+#pragma comment(lib, "ole32.lib") // compbaseapi.h
+
+// defines
+
+
+#endif
